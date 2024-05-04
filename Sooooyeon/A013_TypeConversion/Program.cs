1eddff8207ff14a7b523b58b5ce8283d6901d3cf
test1 <tn7278@naver.com>
2024-05-04 19:56:52 +0900

    Sooooyeon A013

---
+++ Sooooyeon/A013_TypeConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A013_TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2147483647;
+            long bigNum = num; // 암시적 형변환
+            Console.WriteLine(bigNum);
+
+            double x = 1234.5;
+            int a;
+
+            a = (int)x; // 명시적 형변환
+            Console.WriteLine(a);
+        }
+    }
+}
