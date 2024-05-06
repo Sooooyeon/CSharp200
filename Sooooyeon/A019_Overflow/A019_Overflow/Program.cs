43beb463adefa1fdcf8a0d69f539b2501b4e4e12
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-05-07 02:15:38 +0900

    Sooooyeon A019

---
+++ Sooooyeon/A019_Overflow/A019_Overflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A019_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // y = int.MaxValue + 10; // 컴파일 에러 발생
+            int x = int.MaxValue, y = 0;
+
+            checked
+            {
+                try
+                {
+                    y = x + 10;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("int.MaxValue + 10 = {0}", y);
+            // 오버플로우가 발생해 0이 출력됨
+        }
+    }
+}
