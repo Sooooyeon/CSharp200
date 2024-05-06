1e77c399c3d79c4f8a07cee245e17cf789e3884f
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-05-07 02:06:59 +0900

    Sooooyeon A018

---
+++ Sooooyeon/A018_DivideByZero/A018_DivideByZero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A018_DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+
+            try
+            {
+                Console.WriteLine(x / y);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
