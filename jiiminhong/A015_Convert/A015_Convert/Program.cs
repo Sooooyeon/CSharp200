86b3bfb8b651f88fd09fe1b9d8b9ba699d06a491
jiiminhong <chunsim015@gmail.com>
2024-05-07 22:14:59 +0900

    jiiminhong A015

---
+++ jiiminhong/A015_Convert/A015_Convert/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A015_Convert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.Write("첫 번째 숫자를 입력하세요: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("두 번째 숫자를 입력하세요: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+
+            // 2진수, 8진수, 10진수, 16진수로 출력하기
+            short value = short.MaxValue; // Int16.MaxValue
+            Console.WriteLine("\n2진수, 8진수, 10진수, 16진수로 출력하기");
+
+            int baseNum = 2;
+            string s = Convert.ToString(value, baseNum);
+            int i = Convert.ToInt32(s, baseNum);
+            Console.WriteLine("i = {0}, {1, 2}진수 = {2, 16}", i, baseNum, s);
+
+            baseNum = 8;
+            s = Convert.ToString(value, baseNum);
+            i = Convert.ToInt32(s, baseNum);
+            Console.WriteLine("i = {0}, {1, 2}진수 = {2, 16}", i, baseNum, s);
+
+            baseNum = 10;
+            s = Convert.ToString(value, baseNum);
+            i = Convert.ToInt32(s, baseNum);
+            Console.WriteLine("i = {0}, {1, 2}진수 = {2, 16}", i, baseNum, s);
+
+            baseNum = 16;
+            s = Convert.ToString(value, baseNum);
+            i = Convert.ToInt32(s, baseNum);
+            Console.WriteLine("i = {0}, {1, 2}진수 = {2, 16}", i, baseNum, s);
+        }
+    }
+}
