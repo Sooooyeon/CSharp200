85ac494a3e477d9de5ac235729f990d82f1ec8f5
ca970 <ca5522397@gmail.com>
2024-05-07 17:24:53 -0400

    A019_Overflow

---
+++ Hyunjin/A019_Overlflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A019_Overlflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.MinValue, y = 0;
+
+            checked
+            {
+                try
+                {
+                    y = x + 10;
+                }
+                catch(Exception e) {
+                
+                }            
+            }
+                Console.WriteLine("int.MaxValue + 10 = {0}",y);
+           
+        }
+    }
+}
