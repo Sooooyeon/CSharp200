ef9b2f9c9f3d25680c64b79c4a0e52e699ce0bd6
hyonie <dev.hyonie@gmail.com>
2024-05-08 00:01:34 +0900

    Sehyeon A015

---
+++ Sehyeon/A013_TypeConversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A013_TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2147483647;
+            long bigNum = num;  // 암시적 변환
+            Console.WriteLine(bigNum);
+
+            double x = 1234.5;
+            int a;
+
+            a = (int)x; //  명시적 변환
+            Console.WriteLine(a);
+
+        }
+    }
+}
