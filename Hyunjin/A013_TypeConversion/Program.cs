40ac0f73f5150200e7e65a608b17263a648ac3d0
ca970 <ca5522397@gmail.com>
2024-05-07 01:05:46 -0400

    Hyunjin A013

---
+++ Hyunjin/A013_TypeConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A013_TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2147483647;
+            long bigNum = num;
+
+            Console.WriteLine(bigNum);
+
+            double x = 1234.5;
+            int a;
+
+            a = (int)x;
+
+            Console.WriteLine(a);
+
+        }
+
+    }
+}
