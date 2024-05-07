1dd82ea7f24737166422ce84c4203b4eb5f83d63
jiiminhong <chunsim015@gmail.com>
2024-05-07 22:03:39 +0900

    jiiminhong A013

---
+++ jiiminhong/A013_TypeConversion/A013_TypeConversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A013_TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2147483647;
+            long bigNum = num; // 암시적 형변환
+            Console.WriteLine(bigNum);
+
+            double x = 1234.5;
+            int a;
+
+            a = (int)x; // 명시적 형변환
+            Console.WriteLine(a);
+        }
+    }
+}
