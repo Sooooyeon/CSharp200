8d90399c25975a2bdde201c0e390c1ab75255aec
hyonie <dev.hyonie@gmail.com>
2024-05-08 21:52:26 +0900

    Sehyeon A19

---
+++ Sehyeon/A18_DivideByZero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A18_DivideByZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 0;
+
+            try
+            {
+                Console.WriteLine(x / y);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            
+        }
+    }
+}
---
+++ Sehyeon/A19_Overflow/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A19_Overflow
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int.MaxValue = {0}", int.MaxValue);
+
+            int x = int.MaxValue, y = 0;
+            y = x + 10;
+            Console.WriteLine("int.MaxValue + 10 = {0}", y);
+
+        }
+    }
+}
