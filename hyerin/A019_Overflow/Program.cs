17238ae5c2e1285ef524bd555338056eac7652f2
lhr0055 <lhr0055@naver.com>
2024-05-08 13:31:17 +0900

    hyerin A013-A026

---
+++ hyerin/A019_Overflow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A019_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int.MaxValue = {0}", int.MaxValue);
+            int x = int.MaxValue, y = 0; //Int32 최대값 2,147,483,647
+
+            //y = x + 10; //최대값보다 10 오버플로우되어 -2,147,483,639가 출력 
+            //Console.WriteLine("int.MaxValue + 10 = {0}", y);
+
+            checked //오버플로우 예외를 발생한다.안하는 경우 실행이 계속됨 
+            {
+                try
+                {
+                    y = x + 10; //오버플로우 예외
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("int.MaxValue + 10 = {0}", y); //변수 y의 값은 0으로 출력된다. 
+        }
+    }
+}
---
+++ hyerin/A026_SplitMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A026_SplitMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("더하고자 하는 숫자들을 입력하세요: ");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+
+            int sum = 0;
+            string[] v = s.Split(); //엔터전까지 입력 후 공백으로 배열 나누기 
+            foreach(var i in v)
+            {
+                sum += int.Parse(i);
+            }
+            Console.WriteLine("결과는 {0}", sum);
+        }
+    }
+}
