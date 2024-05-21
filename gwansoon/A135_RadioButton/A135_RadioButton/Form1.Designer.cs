a1f780e3848e55b592ddbcb2177383daab3407a1
Elk <137801344+flution@users.noreply.github.com>
2024-05-21 17:55:46 +0900

    gwansoon A133~A136

---
+++ gwansoon/A135_RadioButton/A135_RadioButton/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace A135_RadioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbKorea = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rbChina = new System.Windows.Forms.RadioButton();
+            this.rbJapan = new System.Windows.Forms.RadioButton();
+            this.rbOther = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbKorea
+            // 
+            this.rbKorea.AutoSize = true;
+            this.rbKorea.Location = new System.Drawing.Point(39, 54);
+            this.rbKorea.Name = "rbKorea";
+            this.rbKorea.Size = new System.Drawing.Size(105, 22);
+            this.rbKorea.TabIndex = 2;
+            this.rbKorea.TabStop = true;
+            this.rbKorea.Text = "대한민국";
+            this.rbKorea.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(559, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "제출";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rbChina
+            // 
+            this.rbChina.AutoSize = true;
+            this.rbChina.Location = new System.Drawing.Point(39, 119);
+            this.rbChina.Name = "rbChina";
+            this.rbChina.Size = new System.Drawing.Size(69, 22);
+            this.rbChina.TabIndex = 4;
+            this.rbChina.TabStop = true;
+            this.rbChina.Text = "중국";
+            this.rbChina.UseVisualStyleBackColor = true;
+            // 
+            // rbJapan
+            // 
+            this.rbJapan.AutoSize = true;
+            this.rbJapan.Location = new System.Drawing.Point(39, 181);
+            this.rbJapan.Name = "rbJapan";
+            this.rbJapan.Size = new System.Drawing.Size(69, 22);
+            this.rbJapan.TabIndex = 5;
+            this.rbJapan.TabStop = true;
+            this.rbJapan.Text = "일본";
+            this.rbJapan.UseVisualStyleBackColor = true;
+            // 
+            // rbOther
+            // 
+            this.rbOther.AutoSize = true;
+            this.rbOther.Location = new System.Drawing.Point(39, 243);
+            this.rbOther.Name = "rbOther";
+            this.rbOther.Size = new System.Drawing.Size(117, 22);
+            this.rbOther.TabIndex = 6;
+            this.rbOther.TabStop = true;
+            this.rbOther.Text = "그 외 국가";
+            this.rbOther.UseVisualStyleBackColor = true;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(65, 44);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(69, 22);
+            this.rbMale.TabIndex = 7;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "남성";
+            this.rbMale.UseVisualStyleBackColor = true;
+            this.rbMale.CheckedChanged += new System.EventHandler(this.rbMale_CheckedChanged);
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(192, 44);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(69, 22);
+            this.rbFemale.TabIndex = 8;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "여성";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            this.rbFemale.CheckedChanged += new System.EventHandler(this.rbFemale_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbKorea);
+            this.groupBox1.Controls.Add(this.rbChina);
+            this.groupBox1.Controls.Add(this.rbJapan);
+            this.groupBox1.Controls.Add(this.rbOther);
+            this.groupBox1.Location = new System.Drawing.Point(91, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 303);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "국적";
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.rbMale);
+            this.GroupBox2.Controls.Add(this.rbFemale);
+            this.GroupBox2.Location = new System.Drawing.Point(393, 72);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(320, 100);
+            this.GroupBox2.TabIndex = 10;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "성별";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbKorea;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rbChina;
+        private System.Windows.Forms.RadioButton rbJapan;
+        private System.Windows.Forms.RadioButton rbOther;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox GroupBox2;
+    }
+}
+
