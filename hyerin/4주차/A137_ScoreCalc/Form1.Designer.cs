02296c68a6540e0ab7d4ef1c412bb5a954fbbd3d
lhr0055 <lhr0055@naver.com>
2024-05-24 17:22:19 +0900

    hyering A140

---
+++ hyerin/4주차/A137_ScoreCalc/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace A137_ScoreCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtko = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtma = new System.Windows.Forms.TextBox();
+            this.txten = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.txtAvg = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txten);
+            this.groupBox1.Controls.Add(this.txtma);
+            this.groupBox1.Controls.Add(this.txtko);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(53, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 185);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "성적입력";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtAvg);
+            this.groupBox2.Controls.Add(this.txtSum);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(300, 97);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(221, 139);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "결과";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "국어";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "수학";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "영어";
+            // 
+            // txtko
+            // 
+            this.txtko.Location = new System.Drawing.Point(94, 46);
+            this.txtko.Name = "txtko";
+            this.txtko.Size = new System.Drawing.Size(100, 21);
+            this.txtko.TabIndex = 3;
+            this.txtko.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "총점";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "평균";
+            // 
+            // txtma
+            // 
+            this.txtma.Location = new System.Drawing.Point(94, 79);
+            this.txtma.Name = "txtma";
+            this.txtma.Size = new System.Drawing.Size(100, 21);
+            this.txtma.TabIndex = 4;
+            this.txtma.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txten
+            // 
+            this.txten.Location = new System.Drawing.Point(94, 114);
+            this.txten.Name = "txten";
+            this.txten.Size = new System.Drawing.Size(100, 21);
+            this.txten.TabIndex = 5;
+            this.txten.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSum
+            // 
+            this.txtSum.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtSum.Location = new System.Drawing.Point(94, 43);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(100, 21);
+            this.txtSum.TabIndex = 6;
+            this.txtSum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAvg
+            // 
+            this.txtAvg.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtAvg.Location = new System.Drawing.Point(94, 85);
+            this.txtAvg.Name = "txtAvg";
+            this.txtAvg.Size = new System.Drawing.Size(100, 21);
+            this.txtAvg.TabIndex = 7;
+            this.txtAvg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "계산";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txten;
+        private System.Windows.Forms.TextBox txtma;
+        private System.Windows.Forms.TextBox txtko;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAvg;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ hyerin/4주차/A139_RgnScrollBar/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A139_RgnScrollBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.LightSteelBlue;
+            panel1.BackColor = Color.FromArgb(0, 0, 0);
+            txtR.Text = 0.ToString();
+            txtG.Text = 0.ToString();
+            txtB.Text = 0.ToString();
+            scrR.Maximum = 255 + 9;
+            scrG.Maximum = 255 + 9;
+            scrB.Maximum = 255 + 9;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scrR_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+        }
+
+        private void scr_Scroll(object sender, ScrollEventArgs e)
+        {
+            txtR.Text = scrR.Value.ToString();
+            txtG.Text = scrG.Value.ToString();
+            txtB.Text = scrB.Value.ToString();
+            panel1.BackColor = Color.FromArgb(scrR.Value, scrG.Value, scrB.Value);
+        }
+
+        private void txt_ch(object sender, EventArgs e)
+        {
+            if(txtR.Text != "" && txtG.Text !="" && txtB.Text !="")
+            {
+                scrR.Value = int.Parse(txtR.Text);
+                scrG.Value = int.Parse(txtG.Text);
+                scrB.Value = int.Parse(txtB.Text);
+                panel1.BackColor = Color.FromArgb(scrR.Value, scrG.Value, scrB.Value);
+            }
+        }
+    }
+}
