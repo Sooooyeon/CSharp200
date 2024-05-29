9f2229aa12d2bbd9e04008d89a6cdd88620bbf48
ca970 <ca5522397@gmail.com>
2024-05-29 18:16:44 -0400

    A140 Hyunjin

---
+++ Hyunjin/A140_ListBox/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace A140_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.txtSIndex1 = new System.Windows.Forms.TextBox();
+            this.txtSItem1 = new System.Windows.Forms.TextBox();
+            this.txtSIndex2 = new System.Windows.Forms.TextBox();
+            this.txtSItem2 = new System.Windows.Forms.TextBox();
+            this.txtSIndex3 = new System.Windows.Forms.TextBox();
+            this.txtSItem3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "미국",
+            "러시아",
+            "중국",
+            "영국",
+            "독일",
+            "프랑스",
+            "일본",
+            "이라스라엘",
+            "사우디아라비아",
+            "UAE"});
+            this.listBox1.Location = new System.Drawing.Point(173, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 256);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(425, 25);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(222, 256);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 18;
+            this.listBox3.Location = new System.Drawing.Point(667, 25);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(222, 256);
+            this.listBox3.TabIndex = 2;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // txtSIndex1
+            // 
+            this.txtSIndex1.Location = new System.Drawing.Point(173, 306);
+            this.txtSIndex1.Name = "txtSIndex1";
+            this.txtSIndex1.Size = new System.Drawing.Size(222, 28);
+            this.txtSIndex1.TabIndex = 3;
+            this.txtSIndex1.TabStop = false;
+            // 
+            // txtSItem1
+            // 
+            this.txtSItem1.Location = new System.Drawing.Point(173, 363);
+            this.txtSItem1.Name = "txtSItem1";
+            this.txtSItem1.Size = new System.Drawing.Size(222, 28);
+            this.txtSItem1.TabIndex = 4;
+            // 
+            // txtSIndex2
+            // 
+            this.txtSIndex2.Location = new System.Drawing.Point(425, 306);
+            this.txtSIndex2.Name = "txtSIndex2";
+            this.txtSIndex2.Size = new System.Drawing.Size(222, 28);
+            this.txtSIndex2.TabIndex = 5;
+            // 
+            // txtSItem2
+            // 
+            this.txtSItem2.Location = new System.Drawing.Point(425, 363);
+            this.txtSItem2.Name = "txtSItem2";
+            this.txtSItem2.Size = new System.Drawing.Size(222, 28);
+            this.txtSItem2.TabIndex = 6;
+            // 
+            // txtSIndex3
+            // 
+            this.txtSIndex3.Location = new System.Drawing.Point(667, 306);
+            this.txtSIndex3.Name = "txtSIndex3";
+            this.txtSIndex3.Size = new System.Drawing.Size(222, 28);
+            this.txtSIndex3.TabIndex = 7;
+            // 
+            // txtSItem3
+            // 
+            this.txtSItem3.Location = new System.Drawing.Point(667, 363);
+            this.txtSItem3.Name = "txtSItem3";
+            this.txtSItem3.Size = new System.Drawing.Size(222, 28);
+            this.txtSItem3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "SelectedIndex";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "SelectedItem";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 419);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSItem3);
+            this.Controls.Add(this.txtSIndex3);
+            this.Controls.Add(this.txtSItem2);
+            this.Controls.Add(this.txtSIndex2);
+            this.Controls.Add(this.txtSItem1);
+            this.Controls.Add(this.txtSIndex1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox txtSIndex1;
+        private System.Windows.Forms.TextBox txtSItem1;
+        private System.Windows.Forms.TextBox txtSIndex2;
+        private System.Windows.Forms.TextBox txtSItem2;
+        private System.Windows.Forms.TextBox txtSIndex3;
+        private System.Windows.Forms.TextBox txtSItem3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
