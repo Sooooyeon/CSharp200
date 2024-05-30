fc09018ff2afd1a9429ff8715584ac15aae1ac9b
Taehoon Kim <thk8lue@gmail.com>
2024-05-30 21:49:11 +0900

    Taehoon A140

---
+++ Taehoon/Week 02/A027_StringConcat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace A027_StringConcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "bikang";
+            string date = DateTime.Today.ToShortDateString();
+
+            string strPlus = "Hello " + userName + ". Today is " + date + ".";
+            Console.WriteLine(strPlus);
+
+            string strFormat = String.Format("Hello {0}. Today is {1}.", userName, date);
+            Console.WriteLine(strFormat);
+
+            string strInterpolation = $"Hello {userName}. Today is {date}.";
+            Console.WriteLine(strInterpolation);
+
+            string strConcat = String.Concat("Hello ", userName,
+                ". Today is ", date, ".");
+
+            string[] animals = { "mouse", "cow", "tiger", "rabbit", "dragon" };
+            string s = String.Concat(animals);
+            Console.WriteLine(s);
+            s = String.Join(", ", animals);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Taehoon/Week 03/A033_ConstAndReadonly/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace A033_ConstAndReadonly
+{
+    class ConstEx
+    {
+        public const int number = 3;
+    }
+
+    class ReadonlyEx
+
+    {
+        public readonly int number = 10;
+        public ReadonlyEx()
+        {
+            number = 20;
+        }
+        public ReadonlyEx(int n)
+        {
+            number = n;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConstEx.number);
+            // 3
+
+            ReadonlyEx inst1 = new ReadonlyEx();
+            Console.WriteLine(inst1.number);
+            // 20
+
+            ReadonlyEx inst2 = new ReadonlyEx(100);
+            Console.WriteLine(inst2.number);
+            // 100
+        }
+    }
+}
