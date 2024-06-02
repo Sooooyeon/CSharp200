82ccc4810ad4c7e476916c33c45a6c17db360413
hyonie <dev.hyonie@gmail.com>
2024-06-02 15:38:38 +0900

    Sehyeon A141-A144

---
+++ Sehyeon/A142_CheckedListBox/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace A142_CheckedListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cLstBox = new System.Windows.Forms.CheckedListBox();
+            this.btnDelAll = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cLstBox
+            // 
+            this.cLstBox.FormattingEnabled = true;
+            this.cLstBox.Location = new System.Drawing.Point(96, 60);
+            this.cLstBox.Name = "cLstBox";
+            this.cLstBox.Size = new System.Drawing.Size(217, 244);
+            this.cLstBox.TabIndex = 0;
+            // 
+            // btnDelAll
+            // 
+            this.btnDelAll.Location = new System.Drawing.Point(600, 370);
+            this.btnDelAll.Name = "btnDelAll";
+            this.btnDelAll.Size = new System.Drawing.Size(120, 32);
+            this.btnDelAll.TabIndex = 1;
+            this.btnDelAll.Text = "모두 삭제";
+            this.btnDelAll.UseVisualStyleBackColor = true;
+            this.btnDelAll.Click += new System.EventHandler(this.btnDelAll_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 15;
+            this.lstBox.Location = new System.Drawing.Point(465, 60);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(255, 244);
+            this.lstBox.TabIndex = 2;
+            // 
+            // btnTo
+            // 
+            this.btnTo.Location = new System.Drawing.Point(347, 113);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(83, 32);
+            this.btnTo.TabIndex = 3;
+            this.btnTo.Text = ">";
+            this.btnTo.UseVisualStyleBackColor = true;
+            this.btnTo.Click += new System.EventHandler(this.btnTo_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(347, 164);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(83, 32);
+            this.btnAll.TabIndex = 4;
+            this.btnAll.Text = ">>";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(347, 223);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(83, 32);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.btnDelAll);
+            this.Controls.Add(this.cLstBox);
+            this.Name = "Form1";
+            this.Text = "Traver Wish List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox cLstBox;
+        private System.Windows.Forms.Button btnDelAll;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
+
---
+++ Sehyeon/A143_GradeCalc/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace A143_GradeCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crd1 = new System.Windows.Forms.ComboBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.grd1 = new System.Windows.Forms.ComboBox();
+            this.grd2 = new System.Windows.Forms.ComboBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.crd2 = new System.Windows.Forms.ComboBox();
+            this.grd3 = new System.Windows.Forms.ComboBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.crd3 = new System.Windows.Forms.ComboBox();
+            this.grd4 = new System.Windows.Forms.ComboBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.crd4 = new System.Windows.Forms.ComboBox();
+            this.grd5 = new System.Windows.Forms.ComboBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.crd5 = new System.Windows.Forms.ComboBox();
+            this.grd6 = new System.Windows.Forms.ComboBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.crd6 = new System.Windows.Forms.ComboBox();
+            this.grd7 = new System.Windows.Forms.ComboBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.crd7 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // crd1
+            // 
+            this.crd1.FormattingEnabled = true;
+            this.crd1.Location = new System.Drawing.Point(232, 68);
+            this.crd1.Name = "crd1";
+            this.crd1.Size = new System.Drawing.Size(121, 23);
+            this.crd1.TabIndex = 0;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(92, 66);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 25);
+            this.txt1.TabIndex = 1;
+            // 
+            // grd1
+            // 
+            this.grd1.FormattingEnabled = true;
+            this.grd1.Location = new System.Drawing.Point(403, 68);
+            this.grd1.Name = "grd1";
+            this.grd1.Size = new System.Drawing.Size(121, 23);
+            this.grd1.TabIndex = 2;
+            // 
+            // grd2
+            // 
+            this.grd2.FormattingEnabled = true;
+            this.grd2.Location = new System.Drawing.Point(403, 114);
+            this.grd2.Name = "grd2";
+            this.grd2.Size = new System.Drawing.Size(121, 23);
+            this.grd2.TabIndex = 5;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(92, 112);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 25);
+            this.txt2.TabIndex = 4;
+            // 
+            // crd2
+            // 
+            this.crd2.FormattingEnabled = true;
+            this.crd2.Location = new System.Drawing.Point(232, 114);
+            this.crd2.Name = "crd2";
+            this.crd2.Size = new System.Drawing.Size(121, 23);
+            this.crd2.TabIndex = 3;
+            // 
+            // grd3
+            // 
+            this.grd3.FormattingEnabled = true;
+            this.grd3.Location = new System.Drawing.Point(403, 163);
+            this.grd3.Name = "grd3";
+            this.grd3.Size = new System.Drawing.Size(121, 23);
+            this.grd3.TabIndex = 8;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(92, 161);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 25);
+            this.txt3.TabIndex = 7;
+            // 
+            // crd3
+            // 
+            this.crd3.FormattingEnabled = true;
+            this.crd3.Location = new System.Drawing.Point(232, 163);
+            this.crd3.Name = "crd3";
+            this.crd3.Size = new System.Drawing.Size(121, 23);
+            this.crd3.TabIndex = 6;
+            // 
+            // grd4
+            // 
+            this.grd4.FormattingEnabled = true;
+            this.grd4.Location = new System.Drawing.Point(403, 217);
+            this.grd4.Name = "grd4";
+            this.grd4.Size = new System.Drawing.Size(121, 23);
+            this.grd4.TabIndex = 11;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(92, 215);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 25);
+            this.txt4.TabIndex = 10;
+            // 
+            // crd4
+            // 
+            this.crd4.FormattingEnabled = true;
+            this.crd4.Location = new System.Drawing.Point(232, 217);
+            this.crd4.Name = "crd4";
+            this.crd4.Size = new System.Drawing.Size(121, 23);
+            this.crd4.TabIndex = 9;
+            // 
+            // grd5
+            // 
+            this.grd5.FormattingEnabled = true;
+            this.grd5.Location = new System.Drawing.Point(403, 269);
+            this.grd5.Name = "grd5";
+            this.grd5.Size = new System.Drawing.Size(121, 23);
+            this.grd5.TabIndex = 14;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(92, 267);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 25);
+            this.txt5.TabIndex = 13;
+            // 
+            // crd5
+            // 
+            this.crd5.FormattingEnabled = true;
+            this.crd5.Location = new System.Drawing.Point(232, 269);
+            this.crd5.Name = "crd5";
+            this.crd5.Size = new System.Drawing.Size(121, 23);
+            this.crd5.TabIndex = 12;
+            // 
+            // grd6
+            // 
+            this.grd6.FormattingEnabled = true;
+            this.grd6.Location = new System.Drawing.Point(403, 324);
+            this.grd6.Name = "grd6";
+            this.grd6.Size = new System.Drawing.Size(121, 23);
+            this.grd6.TabIndex = 17;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(92, 322);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(100, 25);
+            this.txt6.TabIndex = 16;
+            // 
+            // crd6
+            // 
+            this.crd6.FormattingEnabled = true;
+            this.crd6.Location = new System.Drawing.Point(232, 324);
+            this.crd6.Name = "crd6";
+            this.crd6.Size = new System.Drawing.Size(121, 23);
+            this.crd6.TabIndex = 15;
+            // 
+            // grd7
+            // 
+            this.grd7.FormattingEnabled = true;
+            this.grd7.Location = new System.Drawing.Point(403, 376);
+            this.grd7.Name = "grd7";
+            this.grd7.Size = new System.Drawing.Size(121, 23);
+            this.grd7.TabIndex = 20;
+            // 
+            // txt7
+            // 
+            this.txt7.Location = new System.Drawing.Point(92, 374);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(100, 25);
+            this.txt7.TabIndex = 19;
+            // 
+            // crd7
+            // 
+            this.crd7.FormattingEnabled = true;
+            this.crd7.Location = new System.Drawing.Point(232, 376);
+            this.crd7.Name = "crd7";
+            this.crd7.Size = new System.Drawing.Size(121, 23);
+            this.crd7.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "평균 평점";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(403, 425);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(121, 25);
+            this.txtGrade.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 477);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grd7);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.crd7);
+            this.Controls.Add(this.grd6);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.crd6);
+            this.Controls.Add(this.grd5);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.crd5);
+            this.Controls.Add(this.grd4);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.crd4);
+            this.Controls.Add(this.grd3);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.crd3);
+            this.Controls.Add(this.grd2);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.crd2);
+            this.Controls.Add(this.grd1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.crd1);
+            this.Name = "Form1";
+            this.Text = "학점 계산기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox crd1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.ComboBox grd1;
+        private System.Windows.Forms.ComboBox grd2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.ComboBox crd2;
+        private System.Windows.Forms.ComboBox grd3;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.ComboBox crd3;
+        private System.Windows.Forms.ComboBox grd4;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.ComboBox crd4;
+        private System.Windows.Forms.ComboBox grd5;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.ComboBox crd5;
+        private System.Windows.Forms.ComboBox grd6;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.ComboBox crd6;
+        private System.Windows.Forms.ComboBox grd7;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.ComboBox crd7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtGrade;
+    }
+}
+
