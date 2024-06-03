e3f4c73992720316a44c5b4890cdd7df0ba47818
lhr0055 <lhr0055@naver.com>
2024-06-03 17:10:57 +0900

    hyerin A144

---
+++ hyerin/5주차/A142_CheckedListBox/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace A142_CheckedListBox
+{
+    partial class form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clstBox = new System.Windows.Forms.CheckedListBox();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clstBox
+            // 
+            this.clstBox.FormattingEnabled = true;
+            this.clstBox.Location = new System.Drawing.Point(100, 89);
+            this.clstBox.Name = "clstBox";
+            this.clstBox.Size = new System.Drawing.Size(120, 84);
+            this.clstBox.TabIndex = 0;
+            // 
+            // btnTo
+            // 
+            this.btnTo.Location = new System.Drawing.Point(276, 89);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(75, 23);
+            this.btnTo.TabIndex = 1;
+            this.btnTo.Text = ">";
+            this.btnTo.UseVisualStyleBackColor = true;
+            this.btnTo.Click += new System.EventHandler(this.btnTo_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(276, 150);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(276, 118);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(75, 23);
+            this.btnAll.TabIndex = 3;
+            this.btnAll.Text = ">>";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 12;
+            this.lstBox.Location = new System.Drawing.Point(395, 89);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstBox.Size = new System.Drawing.Size(120, 88);
+            this.lstBox.TabIndex = 4;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnDel.Location = new System.Drawing.Point(440, 198);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 5;
+            this.btnDel.Text = "모두 삭제";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.clstBox);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clstBox;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
